b517a357992b6b3d77e3cc5d68d612fce08324e5
Hayley Boigenzahn <haboigenzahn@wpi.edu>
2017-06-26 16:10:28 -0500

    Constraints & Output
    Finished building constraints and added code that prints results or outputs them to a file depending on what parts aren't commented out. Compared with GAMS model. There is still a slight difference, but it appears to be rounding error.

--- Task1b_gurobi.py
+++ Task1b_gurobi.py
@@ -9,7 +9,7 @@ from gurobipy import *
 import pandas as pd
 import numpy as np
 
-file = pd.ExcelFile('..\gamsdir\projdir\Task1b.xlsx')
+file = pd.ExcelFile('..\gamsdir\projdir\Task1b_gurobi.xlsx')
 
 mod = Model('Task1b')
 
@@ -19,9 +19,10 @@ feeds = ['PW', 'CS']
 prods = ['EtOH']
 
 #Harvest Sites
-hs_df = file.parse(header=1,skipfooter=677,parse_cols='M')
+hs_df = file.parse(skipfooter=677,parse_cols='M')
 u_hs = hs_df.values.flatten()
-
+hs = [str(k) for k in u_hs]
+print hs
 
 #Biorefineries
 brfs = ['B1','B2','B3','B4']
@@ -50,7 +51,7 @@ for k,v in alpha_temp.iteritems():
 #print alpha
 
 # Demand (liters)
-beta = 4725000
+beta = 47250000
 
 # Material loss factor (per period)
 gamma = {'PW':0.027,'CS':0.015}
@@ -135,10 +136,10 @@ P = mod.addVars(prods,brfs,times,name="P")
 Q = mod.addVars(brfs,techs,name="Q")
 
 # Inventory of biorefinery at the end of period t (10^3 tons)
-Sbr = mod.addVars(cmpds,brfs,times,name="Sbr")
+Sbr = mod.addVars(feeds,brfs,times,name="Sbr")
 
 # Inventory of harvest site at the end of period t (10^3 tons)
-Shs = mod.addVars(cmpds,hs,times,name="Shs") 
+Shs = mod.addVars(feeds,hs,times,name="Shs") 
 
 # Binary selection - will equal 1 if technology m is selected at refinery l
 U = mod.addVars(brfs,techs,vtype=GRB.BINARY,name="U")
@@ -147,14 +148,10 @@ mod.update()
 
 mod.setObjective(TAC,GRB.MINIMIZE)
 
-
-
-
-
 # Check - not sure if double sums are working correctly
 # Feedstock cost (10^3 $)
 for t in times:
-    mod.addConstr(quicksum(H[i,j,t]*llambda[i] for i in feeds for j in hs) == Cfeed[t])
+    mod.addConstrs(quicksum(H[i,j,t]*llambda[i] for i in feeds for j in hs) == Cfeed[t] for t in times)
 # Inventory cost (10^3 $)
 for t in times:
     mod.addConstr((quicksum(Sbr[i,l,t]*iota[i] for i in feeds for l in brfs)+quicksum(Shs[i,j,t]*iota[i] for i in feeds for j in hs)) == Cinv[t])
@@ -165,12 +162,74 @@ for t in times:
 for t in times:
     mod.addConstr(quicksum((kappaf[i]+kappav[i]*tau[(j,l)])*F[i,j,l,t] for i in feeds for j in hs for l in brfs) == Ctrans[t])
 # Annualized capital cost based on daily plant capacity
-mod.addConstr(quicksum((zeta[m]*Q[l,m]/2000)/1000 for m in techs for l in brfs) == Ccapex)
+mod.addConstr(quicksum((zeta[m]*Q[l,m]/2000) for m in techs for l in brfs)/1000 == Ccapex)
 
-#TODO for Monday: Finish entering constraints, test the code, learn to do proper output.
+#Total cost formulation (10^3 $)
+mod.addConstr(quicksum(Cfeed[t]+Cinv[t]+Cprod[t]+Ctrans[t] for t in times)+Ccapex == TAC)
 
+# Mass Balance Constraints
+# Refinery inventories (10^3 tons)
+for i in feeds:
+    for l in brfs:
+        for t in times:
+            if t > 1:
+                mod.addConstr((Sbr[i,l,t-1]*(1-gamma[i])+quicksum(F[i,j,l,t] for j in hs)-quicksum(G[i,l,m,t] for m in techs)) == Sbr[i,l,t])
+            elif t==1:
+                mod.addConstr((Sbr[i,l,t+3]*(1-gamma[i])+quicksum(F[i,j,l,t] for j in hs)-quicksum(G[i,l,m,t] for m in techs)) == Sbr[i,l,t])
 
+# Harvest site inventories (10^3 tons)
+for i in feeds:
+    for j in hs:
+        for t in times:
+            if t > 1:
+                mod.addConstr((Shs[i,j,t-1]*(1-gamma[i])+H[i,j,t]-quicksum(F[i,j,l,t] for l in brfs)) == Shs[i,j,t])
+            elif t==1:
+                mod.addConstr((Shs[i,j,t+3]*(1-gamma[i])+H[i,j,t]-quicksum(F[i,j,l,t] for l in brfs)) == Shs[i,j,t])
+                
+# Cannot harvest more biomass than the site produces (10^3 tons)
+mod.addConstrs(alpha[j,i,t] >= H[i,j,t] for i in feeds for j in hs for t in times)      
 
+# Production must meet or exceed demand (liters)
+for i in prods:
+    for t in times:
+        mod.addConstr(quicksum(P[i,l,t] for l in brfs) >= beta)
+        
+# Amount produced equals amount of feed consumed times yield
+for ip in prods:
+    for l in brfs:
+        for t in times:
+            mod.addConstr(quicksum(eta[i,m]*G[i,l,m,t]*1000 for i in feeds for m in techs) == P[ip,l,t])
+            
+# Capacity constraints
+# Amount consumed must be less than the capacity limit of the technology at the biorefinery (tons per day)
+mod.addConstrs(quicksum(G[i,l,m,t] for i in feeds)*(1000/rho) <= Q[l,m] for l in brfs for m in techs for t in times)
 
+# Lower capacity of technology (tons)
+mod.addConstrs(psil[m]*U[l,m] <= Q[l,m] for l in brfs for m in techs)
 
+# Upper capacity of technology (tons)
+mod.addConstrs(psiu[m]*U[l,m] >= Q[l,m] for l in brfs for m in techs)    
+       
+mod.optimize()
 
+# Print the objective variable
+#print('\nCost: %g' % mod.objVal)
+# Print the values of variable F
+#print(mod.getAttr('x',F))
+    
+# Print the names and values of all variables
+#for v in mod.getVars():
+#   print(v.varName, v.x)
+
+# Write python dictionary results to output file
+#    target = open('results_v1.txt','w')
+#    target.write('\nCost: %g' % mod.objVal)
+#    target.write("\n")
+#    target.write('Display Variables: %s' % mod.getAttr('x',F))
+#    target.close()
+
+# Print results to Excel spreadsheet - further manipulation within the sheet is currently needed to strip punctuation and set delimiters
+F_df = pd.DataFrame.from_dict(mod.getAttr('x',F), 'index')
+writer = pd.ExcelWriter('results_v2_excel.xlsx')
+F_df.to_excel(writer,'Sheet1')
+writer.save()
